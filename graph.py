7202243d266f319a061661eb9b2373b9916a2d3e
Nad-ben <126410331+Nad-ben@users.noreply.github.com>
2024-04-27 00:19:32 +0200

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()  # to avoid redundancy 
+        self.edges = defaultdict(set) # dictionnary
+        
+
+    def add_node(self,node):
+        self.nodes.append(node) # adding a node to the set of nodes
+
+    def add_edge(self, node1, node2, cost, b):  # b is a temporary parameter meant to check if the edge is bidirectional or not ;)
+        self.edges[node1].add((node2,cost))
+        self.nodes.add(node1)
+        self.nodes.add(node2)
+        if b == 1:
+            self.edges[node2].add((node1,cost))
+
+    def get_neighbors(self,node1): #get the list of neighbors in the form of tuple
+        return self.edges[node1]
+    
+    def get_best_neighbor(self, node): #might be modified later for the search algo
+        neighbors = self.get_neighbors(node)
+        best_neighbor = min(neighbors, key=lambda x: x[1]) #iterate over the list of neighbors and get the min cost 
+        return best_neighbor
+  
