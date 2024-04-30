83cd6bc5b37292105d3a5cf85269d95ccc7c9b0f
Marie-55 <145933148+Marie-55@users.noreply.github.com>
2024-05-01 00:17:49 +0100

    Add files via upload

--- csvread.py
+++ csvread.py
@@ -1,41 +1,60 @@
 import csv
 from Node import Node
 from graph import Graph
+from UCS import ucs
+
+from queue import PriorityQueue
+from Problem import Problem
+
 
 
 G = Graph()
 
 all_nodes=[]
-with open('n.csv', 'r') as csv_file:
+with open('nodes.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
     
     
     for line in csv_reader:
-        ID, x, y,name=line #retrieve the data in this order
-        if(name ==""):
-            node=Node(ID,(x,y))
-            all_nodes.append(node)
-        else:
-            node=Node(ID,(x,y),name)
-            all_nodes.append(node)
+        ID, x, y=line #retrieve the data in this order
+        node=Node(int(ID),(x,y))
+        all_nodes.append(node)
             
-with open('docs.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+with open('doc.csv', 'r') as csv_file:   #open the edges file
+    csv_reader = csv.reader(csv_file)    
     next(csv_reader)
     
     for line in csv_reader:
-        source=line[0]
-        target=line[1]
-        cost=line[2]
-        bi=line[5]
+        source=int(line[0])
+        target=int(line[1])
+        cost=float(line[2])
+        bi=int(line[5])
         
-        for s in all_nodes:
-            if(i.id==source):
+        for s in all_nodes:           #go through all nodes in the list 
+            if(s.id==source):         #find source node in the list 
                 for t in all_nodes:
-                    if(t.id==target):
-                        add_edge(i,t,cost,bi)
+                    if(t.id==target):   #when source is found , find target
+                        G.add_edge(s,t,cost,bi)   #the objects are directly added
+                        
+
+#problem
+ini=G.get_node(940)
+print (f"this is initial state:  {ini.id}")
+
+
+goal=G.get_node(944)
+print (f"this is goal state:  {goal.id}")
+
+ourprob=Problem(G,ini,goal)
+path=ucs(ourprob)
+
+#printing the path
+for node in path:
+    print(f"this is node : {node.id}")
+    print("-----------------------------")
+
+    
                         
                         
-                        
-G.printGraph()
+#G.printGraph()
