c1abdb05372760b00542b31bb4cd3939485c58a5
NouhaBazoula <120610797+NouhaBazoula@users.noreply.github.com>
2024-04-30 18:36:41 +0100

    Add files via upload

--- Node.py
+++ Node.py
@@ -2,40 +2,34 @@ from collections import deque
 
 
 class Node:
-    def __init__(self,id, coordinates,name= "intersection"):
-        self.id = id
+    def __init__(self, state, coordinates, heuristic,parent=None, destination=None, cost=0):
         self.coordinates = coordinates
-        self.name = name
-
-        # getters for the coordinates 
-    @property 
-    def long(self):
-        return self.coordinates[0]
-        
-    @property 
-    def lat(self):
-        return self.coordinates[1]
-        
+        self.heuristic = heuristic
+        self.state = state
+        self.parent = parent  # node
+        self.destination = destination
+        self.cost = cost  # (incremented with each newly expanded node)
+        if parent is None:  # root node
+            self.depth = 0  # level in the graph 0 for the root node
+        else:
+            self.depth = parent.depth + 1  # parent level + 1
 
     def __hash__(self):
-        if isinstance(self.id, list):
-            return hash(tuple(map(tuple, self.id)))
+        if isinstance(self.state, list):
+            return hash(tuple(map(tuple, self.state)))
         else:
-            return hash(self.name)
+            return hash(self.state)
 
     def __eq__(self, other):
         if isinstance(other, Node):
-            return other.coordinates == self.coordinates 
+            return other.state == self.state 
         else:
             return False
 
-    def Get_coord(self):
-        return self.coordinates
-""" 
-Might be used somewhere else 
+    
+
     def __gt__(self, other):
         return (self.cost + self.heuristic) > (other.cost + other.heuristic)
 
     def __lt__(self, other):
         return (self.cost + self.heuristic) < (other.cost + other.heuristic)
- """
