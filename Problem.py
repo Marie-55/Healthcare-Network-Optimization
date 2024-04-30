20c00353dbcc201bb43fc551216aeb7e1fa7517c
NouhaBazoula <120610797+NouhaBazoula@users.noreply.github.com>
2024-04-30 17:05:39 +0100

    Add files via upload

--- Problem.py
+++ Problem.py
@@ -1,8 +1,14 @@
+from graph import Graph
 class Problem:
     def __init__(self, graph, initial_state, goal_state):
         self.graph = graph
         self.initial_state = initial_state
         self.goal_state = goal_state
 
-    def is_goal_state(self, state): 
+    def goal_test(self, state): 
         return state == self.goal_state
+    
+    def expand_node(self,node):
+        return self.graph.get_neighbors(node)
+    
+        
